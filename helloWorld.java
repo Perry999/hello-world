5adadb5d93d6e723d0eeff9e51f196a83881108d
Perry999 <58375496+Perry999@users.noreply.github.com>
2024-04-19 13:21:10 -0400

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+//helloWorld.java 4/11/24
+public class helloWorld{
+
+	public static void main(String[] args){
+		System.out.println("This is a Hello World Test");
+		System.out.println("\n" +" This is ver 24.4.11");
+	}
+}	
