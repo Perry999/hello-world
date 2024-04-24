3c4d50be9d6afeaebdd147e44f255a796d01a8ad
Perry999 <58375496+Perry999@users.noreply.github.com>
2024-04-24 16:49:11 -0400

    new package definition

--- helloWorld.java
+++ helloWorld.java
@@ -1,11 +1,9 @@
 //helloWorld.java 4/11/24
+package com.java.resources;
 public class helloWorld{
 
 	public static void main(String[] args){
 		System.out.println("This is a Hello World Test");
-		System.out.println(" This is ver 24.4.11");
-           for(int j = 0; j < 5; j++){		
-		System.out.println("TEST REV.2");
-             }
+		System.out.println("\n" +" This is ver 24.4.11");
 	}
 }	
